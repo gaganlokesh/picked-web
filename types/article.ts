aeeec23a45eb912ed453369eddc86f0158e2af1f
Gagan Lokesh <gaganlokesh@gmail.com>
2021-07-02 20:56:28 +0530

    Define types for Article and Source

---
+++ types/article.ts
@@ -0,0 +1,14 @@
+import { Source } from "./source";
+
+export interface Article {
+  id: number
+  title: string
+  url: string
+  canonicalUrl: string
+  authorName: string
+  imageUrl: string
+  publishedAt: Date
+  imagePlaceholder?: string
+  readTime?: number
+  source?: Source
+}
---
+++ types/source.ts
@@ -0,0 +1,7 @@
+export interface Source {
+  id: number
+  slug: string
+  name: string
+  websiteUrl: string
+  feedUrl?: string
+}
