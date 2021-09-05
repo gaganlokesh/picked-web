59eb165ba378480e034572996d5c8dd0ef3800a4
Gagan Lokesh <gaganlokesh@gmail.com>
2021-09-06 02:43:44 +0530

    Stall articles feed request until login state is known
    - This is to allow the server to respond with appropriate data for logged-in and anonymous users

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import styles from '../styles/Home.module.css';
 import Feed from '../components/Feed';
 import FeedLayout from '../components/layouts/FeedLayout';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function Home() {
+  const { isReady } = useAuth();
+  const requestUrl = isReady ? '/articles' : null;
+
   return (
     <>
-      <Feed requestUrl="/articles" />
+      <Feed requestUrl={requestUrl} />
     </>
   );
 }
