4ee68b31c7ba002e58e34adbe21e58349a056ca9
Gagan Lokesh <gaganlokesh@gmail.com>
2021-10-09 15:38:46 +0530

    Initial support for theming
    - Define Primary and Neutral color schemes
    
    - Use CSS variables for color definitions so that it will be easier to configure for dark mode and other themes in future

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      coolGray: colors.coolGray,
+      emerald: colors.emerald,
+      white: colors.white,
+      primary: {
+        darker: 'var(--primary-darker)',
+        dark: 'var(--primary-dark)',
+        semidark: 'var(--primary-semidark)',
+        DEFAULT: 'var(--primary)',
+        semilight: 'var(--primary-semilight)',
+        light: 'var(--primary-light)',
+        lighter: 'var(--primary-lighter)',
+      },
+      neutral: {
+        darker: 'var(--neutral-darker)',
+        dark: 'var(--neutral-dark)',
+        semidark: 'var(--neutral-semidark)',
+        DEFAULT: 'var(--neutral)',
+        semilight: 'var(--neutral-semilight)',
+        light: 'var(--neutral-light)',
+        lighter: 'var(--neutral-lighter)',
+      },
+    },
     extend: {},
   },
   variants: {
