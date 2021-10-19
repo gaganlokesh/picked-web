ca4090a01b509ffbb9d4aa292e4c95d2ea144caf
Gagan Lokesh <gaganlokesh@gmail.com>
2021-10-19 20:37:43 +0530

    Add new widget to show article suggestions for users
    - Show recently bookmarked articles as suggestions

---
+++ components/widgets/SuggestedReading.tsx
@@ -0,0 +1,38 @@
+import { ReactElement } from 'react';
+import Link from 'next/link';
+import useSWR from 'swr';
+import fetcher from '../../api/fetcher';
+import { useAuth } from '../../contexts/AuthContext';
+import { Article } from '../../types/article';
+import ArticleCard from '../ArticleCard';
+
+const SuggestedReading = (): ReactElement => {
+  const { isLoggedIn } = useAuth();
+  const { data, error } = useSWR<Article[]>(
+    isLoggedIn ? '/bookmarks?per_page=5' : null,
+    fetcher
+  );
+
+  if (!isLoggedIn || error) return <></>;
+
+  return (
+    <div className="py-1 bg-neutral-lightest rounded-xl">
+      <h4 className="px-5 py-3">Suggested reading</h4>
+      {data?.map((article) => (
+        <ArticleCard
+          className="px-5 py-3"
+          key={article.id}
+          variant="minimal"
+          article={article}
+        />
+      ))}
+      <div className="px-5 py-3">
+        <Link href="/bookmarks">
+          <a className="cursor-pointer text-primary">Show more</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SuggestedReading;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Feed from '../components/Feed';
 import FeedLayout from '../components/layouts/FeedLayout';
 import { useAuth } from '../contexts/AuthContext';
 import SuggestedSources from '../components/widgets/SuggestedSources';
+import SuggestedReading from '../components/widgets/SuggestedReading';
 
 export default function Home() {
   const { isReady } = useAuth();
@@ -10,14 +11,17 @@ export default function Home() {
 
   return (
     <>
-      <div className="container mx-auto py-5">
+      <div className="container py-5 mx-auto">
         <div className="flex">
           <main className="flex-1">
             <Feed requestUrl={requestUrl} />
           </main>
-          <div className="mx-10"></div>
-          <aside className="w-96">
-            <SuggestedSources />
+          <div className="hidden mx-5 xl:mx-10 lg:block"></div>
+          <aside className="hidden w-96 lg:block">
+            <div className="grid gap-y-6">
+              <SuggestedSources />
+              <SuggestedReading />
+            </div>
           </aside>
         </div>
       </div>
