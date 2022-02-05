0bc118194d25584a9fd94fdd51667c04cb7f7a02
Gagan Lokesh <gaganlokesh@gmail.com>
2022-02-05 15:48:09 +0530

    Show logged-in user profile image in header
    - Add new user popover menu with option to logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,12 @@
 import { ReactElement } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useAuth } from '../contexts/AuthContext';
 import Button from './Button';
+import { Popover } from '@headlessui/react';
 
 const Header = (): ReactElement => {
-  const { isReady, isLoggedIn, openLoginModal, logout } = useAuth();
+  const { user, isReady, isLoggedIn, openLoginModal, logout } = useAuth();
 
   return (
     <>
@@ -19,10 +21,38 @@ const Header = (): ReactElement => {
               {isReady && !isLoggedIn && (
                 <Button onClick={openLoginModal}>Get started</Button>
               )}
-              {isLoggedIn && (
-                <button className="ml-auto uppercase text-md" onClick={logout}>
-                  Logout
-                </button>
+              {isLoggedIn && !!user && (
+                <>
+                  <div className="text-right top-6">
+                    <Popover className="relative">
+                      <Popover.Button className="flex items-center">
+                        <Image
+                          src={user?.profileImageUrl}
+                          alt={user?.name}
+                          width={45}
+                          height={45}
+                          className="rounded-full"
+                        />
+                      </Popover.Button>
+                      <Popover.Panel className="absolute right-0 z-10 mt-2 bg-white border rounded shadow-lg w-72">
+                        <div className="flex flex-col text-left divide-y divide-neutral-light">
+                          <a className="w-full px-4 py-2 cursor-pointer">
+                            <div className="font-semibold text-md">
+                              {user.name}
+                            </div>
+                            <div className="text-sm">@username</div>
+                          </a>
+                          <a
+                            className="w-full px-4 py-2 cursor-pointer"
+                            onClick={logout}
+                          >
+                            Logout
+                          </a>
+                        </div>
+                      </Popover.Panel>
+                    </Popover>
+                  </div>
+                </>
               )}
             </div>
           </div>
--- types/user.ts
+++ types/user.ts
@@ -2,5 +2,5 @@ export interface User {
   id: number;
   name: string;
   email?: string;
-  imageUrl?: string;
+  profileImageUrl: string;
 }
